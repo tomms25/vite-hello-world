c439c5f07efdaa85cdba3f18a2a7a67673e17b69
tomms25 <tommasobaccano@gmail.com>
2022-12-11 11:08:43 +0100

    AppTitle

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data() {
+    return {
+      saluto: 'La mia prima app con Vite!',
+    }
+  }
+}
+</script>
+
+<template>
+
+  <h1> {{ saluto }} </h1>
+
+</template>
+
+<style scoped>
+h1 {
+  background-color: aquamarine;
+  color: white;
+}
+</style>
